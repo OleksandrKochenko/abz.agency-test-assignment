4ed0288ee725584bc305272175104ba8b92a4f9c
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2023-11-14 02:25:01 +0200

    cloudinary + user registration finished

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -2,11 +2,15 @@ const bcrypt = require("bcryptjs");
 const { nanoid } = require("nanoid");
 const jwt = require("jsonwebtoken");
 const tinify = require("tinify");
+const fs = require("fs/promises");
+const cloudinary = require("../helpers/cloudinary");
 require("dotenv").config();
 const { SECRET_KEY, TINIFY_API_KEY } = process.env;
 
 const User = require("../models/user");
+const Position = require("../models/position");
 const RegistrationToken = require("../models/registration-token");
+const httpError = require("../helpers/http-error");
 
 const registrationToken = async (req, res, next) => {
   try {
@@ -24,8 +28,13 @@ const registrationToken = async (req, res, next) => {
 
 const userRegistration = async (req, res, next) => {
   try {
-    const body = req.body;
-    const file = req.file;
+    const { body, file } = req;
+
+    const existedUser = await User.findOne({
+      $or: [{ email: body.email }, { phone: body.phone }],
+    });
+    if (existedUser)
+      throw httpError(409, "User with this phone or email already exist");
 
     tinify.key = TINIFY_API_KEY;
     const source = tinify.fromFile(file.path);
@@ -34,26 +43,29 @@ const userRegistration = async (req, res, next) => {
       width: 70,
       height: 70,
     });
-    const newPath = file.destination + "\\" + "thumb_" + file.filename;
-    resized.toFile(newPath);
+    await resized.toFile(file.path);
 
-    /* const user = await User.findOne({ email });
-    if (user) throw HttpError(409, "Email already in use");
+    const fileData = await cloudinary.uploader.upload(file.path, {
+      folder: "users",
+    });
+    const photo = fileData.url;
+    await fs.unlink(file.path);
 
-    const hashedPswrd = await bcrypt.hash(password, 10); */
+    const password = await bcrypt.hash(body.password, 10);
 
-    /* const avatarLink =
-      avatarBaseUrl + `?name=${name.replace(/ /g, "+")}` + avatarSettings;
- */
-    /* const newUser = await User.create({
-      ...req.body,
-      password: hashedPswrd, */
-    //avatarURL: avatarLink,
-    /* }); */
+    const { title } = await Position.findById(body.position_id);
+
+    const { _id: user_id } = await User.create({
+      ...body,
+      photo,
+      password,
+      position: title,
+    });
 
     res.status(201).json({
-      body,
-      file,
+      success: true,
+      user_id,
+      message: "New user successfully registered",
     });
   } catch (error) {
     next(error);
